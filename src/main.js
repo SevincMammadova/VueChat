65a28ac04f5db8d19da243c2b9d453d82de096d1
SevincMammadova <sevincmammadova609@gmail.com>
2022-09-24 22:32:17 +0400

    some routing configs done

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./assets/tailwind.css";
+import router from "./router/router";
 
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
-app.mount('#app')
+app
+.use(pinia)
+.use(router)
+.mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+// import Main from "@/pages";
+import {createRouter, createWebHistory} from "vue-router";
+
+
+const routes = [
+    {
+        path: '/',
+        // component: Main
+    },
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL)
+})
+
+export default router;
